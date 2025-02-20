be7d280d1cbf5901745d20a169b715f2c47527e3
Rayan <wrayo44@gmail.com>
2025-02-20 17:57:48 -0500

    Construct prompt and send it to llm

--- logdensitytool/src/providers/analyzeFileProvider.js
+++ logdensitytool/src/providers/analyzeFileProvider.js
@@ -2,7 +2,12 @@ const vscode = require('vscode');
 const path = require('path');
 const { analyzeFiles } = require('../services/analyzeProject');
 const { readFile } = require('../utils/fileReader');
-const { runModel } = require('../services/runModelService')
+const { runModel } = require('../services/runModelService');
+const { createApiModel, createResponse } = require('../services/factoryService');
+const { buildPrompt, getSurroundingMethodText, extractAttributesFromPrompt } = require("../utils/modelTools");
+const { configuration } = require('../model_config');
+const { api_id, url, port, prompt_file, default_model, default_token, llm_temperature, llm_max_token, response_id, attributes_to_comment, comment_string, injection_variable } = configuration;
+
 
 class AnalyzeFileProvider {
     constructor() {
@@ -11,6 +16,7 @@ class AnalyzeFileProvider {
         this.analyzeList = new Map();
         this.remoteUrl = '';
         this.javaFileProvider = null;
+        this.apiModelService = createApiModel(api_id, url, port, default_model, default_token);
     }
 
     refresh() {
@@ -101,24 +107,89 @@ class AnalyzeFileProvider {
         }
     }
 
-    async sendBlocsToLLM() {
+    async getBlocks() {
         const results = await this.sendFilesForAnalysis();
-        const allBlocks = [];
-        //const filteredFiles = results.filter(f => f.density < f.predictedDensity);
-
-        for (let index = 0; index < results.length; index++) {
-            const element = results[index];
+        
+        const allBlocks = await Promise.all(results.map(async (element) => {
             const fileContent = await readFile(element.url);
             const result = await runModel(this.remoteUrl, fileContent);
-    
-            allBlocks.push({ fileName: element.url, fileContent: fileContent, blocks: result.blocks });
-        }
-        
-        console.log("All Blocks: " + JSON.stringify(allBlocks, null, 2));
+            return { filePath: element.url, fileContent, blocks: result.blocks };
+        }));
 
-        // Appeller le LLM et voir ce qu'il raconte
-        
-        //console.log("Filtered files :" + filteredFiles);
+        return allBlocks;
+    }
+
+    async sendBlocksToLLM(blocks) {
+        vscode.window.withProgress({
+            location: vscode.ProgressLocation.Notification,
+            title: "Sending blocks to LLM",
+            cancellable: false
+        }, async (progress) => {
+            progress.report({ message: "Contacting LLM..." });
+
+            try {
+                const model = await this.apiModelService.getModel();
+
+                const projectBasePath = path.resolve(__dirname, "..", "..", "..");
+                let system_prompt = await readFile(path.join(projectBasePath, "prompt", "add_missing_logs.txt"))
+
+                let attributes = []
+                if (system_prompt.includes("{{") && system_prompt.includes("}}")) {
+                    attributes = extractAttributesFromPrompt(system_prompt, attributes_to_comment)
+                    system_prompt = system_prompt.replace("{{", "{");
+                    system_prompt = system_prompt.replace("}}", "}");
+                }
+                
+                const builtPrompt = buildPrompt(JSON.stringify(blocks), system_prompt, "blocks");
+
+                console.log("Prompt : " + JSON.stringify(builtPrompt));
+                
+                const modelResponse = await this.apiModelService.generate(model, null, builtPrompt, llm_temperature, llm_max_token);
+
+                console.log("Reponse From LLM : " + modelResponse);
+
+                // let linesToInsert = [];
+                // while (linesToInsert.length === 0) {
+                //     console.log("Generating log advice...");
+                    
+                //     const modelResponse = await apiModelService.generate(model, null, builtPrompt, llm_temperature, llm_max_token);
+                    
+                //     if (attributes.length > 0) {
+                //         linesToInsert = reponseService.extractLines(modelResponse, attributes, attributes_to_comment, comment_string);
+                //     } else {
+                //         const standardResponse = createResponse(StandardResponse.responseId)
+                //         linesToInsert = standardResponse.extractLines(modelResponse, attributes, attributes_to_comment, comment_string);
+                //     }
+                    
+                // }
+
+                //let cursorPosition = editor.selection.active;
+
+                // Detect indentation style based on the current line
+                // const currentLineText = document.lineAt(cursorPosition.line).text;
+                // const lineIndentMatch = currentLineText.match(/^\s*/); // Match leading whitespace (spaces or tabs)
+                // const detectedIndent = lineIndentMatch ? lineIndentMatch[0] : ''; // Preserve tabs or spaces
+
+                // const edit = new vscode.WorkspaceEdit();
+
+                // for (let i = 0; i < linesToInsert.length; i++) {
+                //     let lineText = linesToInsert[i];
+
+                //     // Preserve the detected indentation for all lines after the first
+                //     const formattedLine = i > 0 ? detectedIndent + lineText : lineText;
+
+                //     // Insert the formatted line
+                //     edit.insert(document.uri, cursorPosition, formattedLine + '\n');
+                // }
+
+                // Apply the edit
+                //await vscode.workspace.applyEdit(edit);
+
+            } catch (error) {
+                console.log(error);
+                vscode.window.showErrorMessage("Failed to get code suggestion. " + error.message);
+            }
+        });
     }
 }
 
@@ -147,10 +218,9 @@ function registerAnalyzeFileProvider(context) {
         console.log(results);
     }));
 
-    context.subscriptions.push(vscode.commands.registerCommand('analyzeFileProvider.improveLogs', async () => {
-        const results = await analyzeFileProvider.sendBlocsToLLM();
-        vscode.window.showInformationMessage('Files sent for log improvements.');
-        //console.log(results);
+    context.subscriptions.push(vscode.commands.registerCommand('analyzeFileProvider.analyzeAndAddMissingLogs', async () => {
+        const blocks = await analyzeFileProvider.getBlocks();
+        await analyzeFileProvider.sendBlocksToLLM(blocks);
     }));
     
     return analyzeFileProvider;  

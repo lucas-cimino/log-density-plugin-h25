ec544ea21e230190dbe6d28e6f07ca85788c3ce3
Andrew-Chedid <andrew.chedid.1@ens.etsmtl.ca>
2025-03-07 16:50:14 -0500

    use llama3.2:3b

--- logdensitytool/src/services/apiModel/queryOllama.js
+++ logdensitytool/src/services/apiModel/queryOllama.js
@@ -2,7 +2,7 @@ const OllamaApiModel = require('./ollamaApiModelService');
 
 const OLLAMA_URL = "http://localhost";
 const OLLAMA_PORT = 11434;
-const MODEL = "llama2:3b"; // Change selon le modèle dispo
+const MODEL = "llama3.2:3b"; // Change selon le modèle dispo
 const PROMPT = "Explique-moi comment fonctionne GitHub Actions.";
 
 async function runQuery() {
@@ -11,12 +11,10 @@ async function runQuery() {
     try {
         // Check if the model is available
         const modelsInfo = await ollama.info();
-        console.log("models:" + modelsInfo.model);
-        //MODEL = modelsInfo.model;
-        // if (!modelsInfo.model.includes(MODEL)) {
-        //     console.log(`Model ${MODEL} not found. Pulling it now...`);
-        //     await ollama.changeModel(MODEL);
-        // }
+        if (!modelsInfo.model.includes(MODEL)) {
+            console.log(`Model ${MODEL} not found. Pulling it now...`);
+            await ollama.changeModel(MODEL);
+        }
 
         // Generate text
         const response = await ollama.generate(MODEL, "", PROMPT, 0.8, 128);
